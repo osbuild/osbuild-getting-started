136f1ea688e287f11f1374cce06a68ffaa6c2c93
Florian Schüller <florian.schueller@redhat.com>
2024-05-15 14:23:07 +0200

    speedup git_stack

--- git_stack.sh
+++ git_stack.sh
@@ -9,20 +9,51 @@ ARGS=${*:-status --branch --short}
 
 NUM="$(echo $REPOS | wc -w)"
 I=0
+running_jobs=0
+MAX_PARALLEL=4
+tmp_files=()
+
+function run_git_command {
+  local dir=$1
+  local args=$2
+  local tmp_file=$3
+  git -c color.status=always -C "$dir" $args >> "$tmp_file" 2>&1 &
+}
+
 for D in $REPOS; do
   I=$(( $I + 1 ))
-  echo ""
-  echo "  ------------ ( $I / $NUM ) $D ------------"
   FULL_DIR="$BASEDIR/$D"
   if [ "$ARGS" == "walk" ]; then
+    echo "  ------------- ( $I / $NUM ) $D -------------"
     pushd $FULL_DIR
     git status
     bash
     popd
   else
-    git -C "$FULL_DIR" $ARGS
+    tmp_file=$(mktemp)
+    tmp_files+=("$tmp_file")
+    echo "  ------------- ( $I / $NUM ) $D -------------" > "$tmp_file"
+    echo "  -- Started -- ( $I / $NUM ) $D -------------"
+    run_git_command "$FULL_DIR" "$ARGS" "$tmp_file"
+
+    (( running_jobs++ ))
+    if (( running_jobs >= MAX_PARALLEL )); then
+      wait -n
+      (( running_jobs-- ))
+    fi
   fi
 done
-if [ "$ARGS" == "walk" ]; then
-  echo "  ------------ DONE ------------"
+
+if [ "$ARGS" != "walk" ]; then
+  wait
 fi
+
+echo "  ------------- DONE -------------"
+
+# Print the buffered output
+for tmp_file in "${tmp_files[@]}"; do
+  echo ""
+  cat "$tmp_file"
+  rm "$tmp_file"
+done
+
